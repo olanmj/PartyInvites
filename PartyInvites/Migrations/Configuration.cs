ca9a581c9aaa869e59e7ea18e2b75e39d431c388
olanmj <olanm@stockton.edu>
2014-03-06 02:18:28 -0500

    Added Gridmvc

--- PartyInvites/Migrations/Configuration.cs
+++ PartyInvites/Migrations/Configuration.cs
@@ -16,18 +16,7 @@ namespace PartyInvites.Migrations
 
         protected override void Seed(PartyInvites.Models.ResponseDBContext context)
         {
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
             context.Responses.AddOrUpdate(
                 r => r.ID,
                 new GuestResponse { Name = "Party Animal", Phone = "1111111", WillAttend = true },
